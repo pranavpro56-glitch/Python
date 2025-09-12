4a484813340a42c33a8fc2dcae006c1f8bb26b0b
titan <pranavpro56@gmail.com>
2025-09-12 13:28:31 +0530

    Add files via upload

---
+++ arthematic operations.py
@@ -0,0 +1,9 @@
+a = 10
+b = 3
+print(a + b) # 13 (addition)
+print(a - b) # 7 (subtraction)
+print(a * b) # 30 (multiplication)
+print(a / b) # 3.333... (division)
+print(a // b) # 3 (floor division)
+print(a % b) # 1 (modulus)
+print(a ** b) # 1000 (exponentiation)
