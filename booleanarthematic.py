a652e4d50c6dcafae1a90b755ff1f80a89ab2543
JARAJAPU SRINAGA YAGNA PRANAV <pranavpro56@gmail.com>
2025-09-13 09:26:36 +0530

    Add files via upload

---
+++ booleanarthematic.py
@@ -0,0 +1,8 @@
+age = 25
+print(age == 25) # True
+print(age != 30) # True
+print(age > 18) # True
+print(age <= 21) # False
+# Chaining Comparisons (A powerful Python feature)
+x = 10
+print(5 < x < 15) # True 
